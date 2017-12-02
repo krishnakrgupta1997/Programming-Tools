6645aa9ac338356f7968d6ce8d3f99aadc824966
KRISHNA KUMAR GUPTA <krishnakrgupta1997@gmail.com>
2017-12-02 11:10:49 +0530

    Create FAST_STDIO.cpp

---
+++ FAST_STDIO.cpp
@@ -0,0 +1,27 @@
+//Fastest method for STDIO the 'int' datatype.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void fastscan(int &x)
+    {
+        bool neg=false;
+        register int c;
+        x =0;
+        c=getchar();
+        if(c=='-')
+        {
+            neg = true;
+            c=getchar();
+        }
+        for(;(c>47 && c<58);c=getchar())
+            x = (x<<1) + (x<<3) +c -48;
+        if(neg)
+            x *=-1;
+    }
+main(){
+  std::ios_base::sync_with_stdio(false);
+  int varint;
+  fastscan(varfloat);
+  cout<<varint<<endl;
+}    
